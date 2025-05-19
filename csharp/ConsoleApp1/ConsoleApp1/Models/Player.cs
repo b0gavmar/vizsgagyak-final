f307982e47ab71bceee7fee8912d65c99f297a4c
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-19 19:01:16 +0200

    jatekos osztaly

--- csharp/ConsoleApp1/ConsoleApp1/Models/Player.cs
+++ csharp/ConsoleApp1/ConsoleApp1/Models/Player.cs
@@ -7,9 +7,68 @@ public partial class Player
 {
     public int? Id { get; set; }
 
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
-    public string? Email { get; set; }
+    public string Email { get; set; }
 
-    public int? Amount { get; set; }
+    private int Amount { get; set; }
+
+    public Player(string name, string email, int amount = 0, int? id = null)
+    {
+        if(string.IsNullOrEmpty(name)) throw new ArgumentNullException("Hiányzik a név!");
+        if(string.IsNullOrEmpty(email)) throw new ArgumentNullException("Hiányzik az email!");
+        if (!email.Contains("@")) throw new ArgumentException("Nem megfelelő email formátum!");
+        this.Name = name;
+        this.Email = email;
+        this.Amount = amount;
+        this.Id = id;
+    }
+
+    public bool HasAmount()
+    {
+        if (Amount > 0)
+        {
+            return true;
+        }
+        else 
+        { 
+            return false; 
+        }
+    }
+
+    public int GetAmount()
+    {
+        if (HasAmount())
+        {
+            return (int)Amount;
+        }
+        else
+        { 
+            return 0; 
+        }
+    }
+
+    public void AddAmount(int num)
+    {
+        if (num > 0)
+        {
+            Amount += num;
+        }
+        else
+        {
+            throw new ArgumentException("Az összeg nem lehet negativ");
+        }
+    }
+
+    public void Fogadas(int num)
+    {
+        if (HasAmount() && num!>Amount)
+        {
+                Amount -= num;
+        }
+        else
+        {
+            throw new ArgumentException("Nincs elég összeg");
+        }
+    }
 }
