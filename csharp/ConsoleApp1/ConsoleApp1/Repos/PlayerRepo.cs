644d979ba0465e2100c25b3b8d80ee4f0a335898
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-19 19:03:53 +0200

    repo osztály kész

---
+++ csharp/ConsoleApp1/ConsoleApp1/Repos/PlayerRepo.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Repos
+{
+    public class PlayerRepo
+    {
+        private readonly PlayersContext _playersContext = new PlayersContext();
+
+        public List<Player> GetNumberOfPlayersWithAmount()
+        {
+            return _playersContext.Players.Where(p=>p.GetAmount() > 0).ToList();
+        }
+
+        public int GetSumOfAllAmount()
+        {
+            return _playersContext.Players.Sum(p => p.GetAmount());
+        }
+    }
+}
